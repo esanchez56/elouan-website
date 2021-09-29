8694c182fee66a04d80b113675413c2f8f32b9b3
RobinPourtaud <pourtaud@outlook.fr>
2021-09-29 12:18:48 +0200

    a

---
+++ first.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+echo "Assistant Cristalea"
+echo "Que voulez-vous faire ?"
+echo " "
+echo " "
+echo "1 - Preview du site en local"
+echo "2 - Construire le site et rendre public"
+cd elouan-website
+read ans
+
+if [ $ans -eq 1 ]; then
+echo " "
+echo "Preview ici:  http://localhost:1313 CTRL+C pour quitter"
+hugo server -d docs > .temp
+elif [ $ans -eq 2 ]; then 
+echo "Construction du site en cours"
+hugo -d docs > .temp
+echo "Construction terminée"
+echo "Préparation de l'envoie"
+git pull
+git commit -a "Modification Article Elouan" 
+echo "Envoie en cours"
+git push master 
+echo "Envoyé ! Le site sera mis à jours sur internet dans les 3 prochaines minutes."
+fi
+
