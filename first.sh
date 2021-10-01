51bcc1db712a8d2680d516f92c777dff78243f19
esanchez56 <sanchezelou@gmail.com>
2021-10-01 18:47:12 +0200

    Modification Article Elouan

--- first.sh
+++ first.sh
@@ -18,8 +18,8 @@ echo "Construction du site en cours"
 hugo -d docs > .temp
 echo "Construction terminée"
 echo "Préparation de l'envoie"
-git pull >> .temp
 git config pull.rebase false >> .temp
+git pull >> .temp
 git add -A >> .temp
 git commit -m "Modification Article Elouan" 
 echo "Envoie en cours"
